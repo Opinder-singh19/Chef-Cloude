1cf1cee5649570c4cdc380cfeb99f5ac600e17bb
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-11 21:41:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import chefcloud from './assets/chefcloude-project-3-removebg-preview.png'
+
 import './App.css'
 import Header from './Components/Header.jsx'
 import Main from './Components/Main.jsx'
