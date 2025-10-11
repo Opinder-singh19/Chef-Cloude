ad4021990ff12cb9e157c8e88661c96ae099671f
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-11 21:47:13 +0530

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
-export default function Header(){
-    return(
-        
-            <div className="logo">
-                <img src="../assets/chefcloude-project-3-removebg-preview.png" alt="" />
-                <h1>CHEF CLOUD</h1>
-            </div>
-    )
-} 
+// src/components/Header.jsx
+import chefcloud from "../assets/chefcloude-project-3-removebg-preview.png";
+
+export default function Header() {
+  return (
+    <div className="logo">
+      <img src={chefcloud} alt="CHEF CLOUD" />
+      <h1>CHEF CLOUD</h1>
+    </div>
+  );
+}
