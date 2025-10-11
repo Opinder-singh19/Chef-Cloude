34d5a62529ff5477b442b3383798dac962de14ea
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-11 17:35:01 +0530

    updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         
             <div className="logo">
-                <img src="/src/assets/chefcloude-project-3-removebg-preview.png" alt="" />
+                <img src="/src/assets/chefcloude-project-3-removebg-preview.png"alt="" />
                 <h1>CHEF CLOUD</h1>
             </div>
     )
