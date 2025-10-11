07703fbeaa016038ed90d39e831a7324ad9a5291
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-11 23:09:59 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/chef-cloud-project/', // 👈 Add this line
+  base: '/chef-cloud-project/', // IMPORTANT
+  plugins: [react()]
 })
