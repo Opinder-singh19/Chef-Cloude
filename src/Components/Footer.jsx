d1ccda4a8d7597b7308f805cb2ffbc2ff3657ed1
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-11 21:48:24 +0530

    UPDATE

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <div className="footer">
            <h3> MADE BY - OPINDER SINGH </h3>
-           <h3> Copyright</h3>
+           <h3> COPYRIGHT@2025</h3>
         </div>
     )
 }
