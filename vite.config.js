a52f53bf605551a1516598807631e1b830dbba60
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-12 21:46:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/chef-cloud-project/', // IMPORTANT
-  plugins: [react()]
+  
+  plugins: [react()],
+  base:"/chef-cloude",
+
 })
