d5df0ebbace4815066b5e7ee1a04a8dad0776644
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-10-12 13:33:25 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer(){
     return(
         <div className="footer">
-           <h3> MADE BY - OPINDER SINGH </h3>
-           <h3> COPYRIGHT@2025</h3>
+           <h3> Terms and Conditions | Privacy Policy | Disclaimer</h3>
+           <h3> &copy; 2025 CHEF CLOUD. All rights reserved. | <strong>Design & Developed by Opinder Singh</strong></h3>
         </div>
     )
 }
